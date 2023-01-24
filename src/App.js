5fdffe121fec934a17700725e7bf395587cb0abf
IveryMikaila <112774755+IveryMikaila@users.noreply.github.com>
2023-01-23 22:02:10 -0500

    Changed the color theme on homepage
    + Added Bootstrap svg symbol
    + Changed the Homepage theme colors to CSS variable colors
    +Added personalized logo from Figma

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-  <div className="spaceHolder"/>
 <div className="container-fluid mainSection">
     <ul className="mynkList">
    <li>ABOUT</li>
@@ -16,11 +15,16 @@ function App() {
    </ul>
    <div className="description-container">
   <div className="description ">
+    <h1>
+      ABOUT
+    </h1>
+    <p>
   Lorem ipsum dolor sit amet. Qui galisum enim At fugiat consequuntur sit modi quam. Aut quaerat iusto sit totam odio aut neque facilis et nesciunt blanditiis est voluptatem mollitia aut neque praesentium sed impedit aperiam. Ut reiciendis quas aut accusantium repudiandae non quaerat tenetur est quia rerum ut voluptas sint qui dolor officiis est quasi omnis? Eos adipisci dolore non eius assumenda aut praesentium galisum in molestiae alias aut soluta expedita et quia iusto.
 
 Ut odio aliquam qui omnis fugit sed blanditiis sequi aut aliquid voluptas aut dicta magnam. Est dolores tempora sit assumenda animi aut quaerat aperiam ut nobis iure ex omnis quaerat eum quia perspiciatis.
 
 Qui natus placeat aut dolor eligendi et sapiente galisum et blanditiis nemo et voluptatem labore qui dolorum corporis. Qui ipsam nostrum id magnam sequi ad odit ipsam qui consequatur culpa. Qui sint natus et eveniet asperiores sed totam minus non optio eveniet hic magni distinctio.
+</p>
   </div>
  
   </div>
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,12 +1,12 @@
 import React from "react";
-
+import mynkLogo from "./img/logo.png"
 
 function NavBar(){
     return (
         <div>
-     <nav class="navbar mynkNavBar bg-body-tertiary fixed-top">
+     <nav class="navbar mynkNavBar bg-body-tertiary ">
   <div class="container-fluid">
-    <a class="navbar-brand" href="/">Mikaila Ivery</a>
+    <a class="navbar-brand" href="/"><img src={mynkLogo} width="200px" alt="Mikaila Ivery's Logo" /></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -27,12 +27,17 @@ function NavBar(){
             <a class="nav-link" href="https://github.com/IveryMikaila?tab=repositories" target="_blank">GITHUB</a>
           </li>
         </ul>
-        <span>
-      <img alt="symbol of downloading file from internet"  />
-      <button className="resumeBtn">
-        <a href="#" className="resumeLink">Resume</a>
+       
+   
+      <button>
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+  <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
+  <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
+</svg>
       </button>
-      </span>
+      <button className="resumeBtn">
+        <a href="#" className="resumeLink"> Resume</a>
+      </button>
       </div>
     </div>
   </div>
