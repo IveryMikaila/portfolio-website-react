0c92116c34bd4b715e9a467c867d065b8383395b
IveryMikaila <112774755+IveryMikaila@users.noreply.github.com>
2023-01-24 20:43:47 -0500

    Added more Javascript animations to description headings

--- src/App.js
+++ src/App.js
@@ -7,18 +7,26 @@ function App() {
   function updateAbout () {
     let description = document.getElementById('description');
     description.style.backgroundColor = "#FCF038"
+    let h1 = document.getElementById('h1');
+    h1.innerHTML ="ABOUT"
   }
 function updateProject () {
   let description = document.getElementById('description');
   description.style.backgroundColor = "#0C24FB"
+  let h1 = document.getElementById('h1');
+  h1.innerHTML ="PROJECTS"
 }
 function updateResume () {
   let description = document.getElementById('description');
   description.style.backgroundColor = "#FCF038"
+  let h1 = document.getElementById('h1');
+    h1.innerHTML ="RESUME"
 }
 function updateContact () {
 let description = document.getElementById('description');
 description.style.backgroundColor = "#0C24FB"
+let h1 = document.getElementById('h1');
+    h1.innerHTML ="CONTACT"
 }
 
   return (
@@ -35,7 +43,7 @@ description.style.backgroundColor = "#0C24FB"
 </div>
 <div className="col-2 right" id="description">
 <p>
-  <h1>ABOUT</h1>
+  <h1 id="h1">ABOUT</h1>
   Lorem ipsum dolor sit amet. Qui galisum enim At fugiat consequuntur sit modi quam. Aut quaerat iusto sit totam odio aut neque facilis et nesciunt blanditiis est voluptatem mollitia aut neque praesentium sed impedit aperiam. Ut reiciendis quas aut accusantium repudiandae non quaerat tenetur est quia rerum ut voluptas sint qui dolor officiis est quasi omnis? Eos adipisci dolore non eius assumenda aut praesentium galisum in molestiae alias aut soluta expedita et quia iusto.
 </p>
 </div>
