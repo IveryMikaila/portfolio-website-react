27eb341087efa43ed662a3fbd75133d35914aa4b
IveryMikaila <112774755+IveryMikaila@users.noreply.github.com>
2023-01-24 20:24:33 -0500

    Added CSS and Javascript Animations
    +Using CSS Hover Pseudo Selector I added an effect where when you hover over the list item it changes the size of it
    +Using Javascript Functions I added an animation that changes the background color of the description when you hover over the list item

--- src/App.js
+++ src/App.js
@@ -3,34 +3,46 @@ import NavBar from "./NavBar"
 
 
 function App() {
+
+  function updateAbout () {
+    let description = document.getElementById('description');
+    description.style.backgroundColor = "#FCF038"
+  }
+function updateProject () {
+  let description = document.getElementById('description');
+  description.style.backgroundColor = "#0C24FB"
+}
+function updateResume () {
+  let description = document.getElementById('description');
+  description.style.backgroundColor = "#FCF038"
+}
+function updateContact () {
+let description = document.getElementById('description');
+description.style.backgroundColor = "#0C24FB"
+}
+
   return (
     <div className="App">
       <NavBar />
-<div className="container-fluid mainSection">
-    <ul className="mynkList">
-   <li id="liOne">ABOUT</li>
-   <li id="liTwo">PROJECTS</li>
-   <li id="liThree">RESUME</li>
-   <li id="liFour">CONTACT</li>
-   </ul>
-   <div className="description-container" id="description-container">
-    
-  <div className="description ">
-    <h1>
-      ABOUT
-    </h1>
-    <p>
-  Lorem ipsum dolor sit amet. Qui galisum enim At fugiat consequuntur sit modi quam. Aut quaerat iusto sit totam odio aut neque facilis et nesciunt blanditiis est voluptatem mollitia aut neque praesentium sed impedit aperiam. Ut reiciendis quas aut accusantium repudiandae non quaerat tenetur est quia rerum ut voluptas sint qui dolor officiis est quasi omnis? Eos adipisci dolore non eius assumenda aut praesentium galisum in molestiae alias aut soluta expedita et quia iusto.
-
-Ut odio aliquam qui omnis fugit sed blanditiis sequi aut aliquid voluptas aut dicta magnam. Est dolores tempora sit assumenda animi aut quaerat aperiam ut nobis iure ex omnis quaerat eum quia perspiciatis.
-
-Qui natus placeat aut dolor eligendi et sapiente galisum et blanditiis nemo et voluptatem labore qui dolorum corporis. Qui ipsam nostrum id magnam sequi ad odit ipsam qui consequatur culpa. Qui sint natus et eveniet asperiores sed totam minus non optio eveniet hic magni distinctio.
-</p>
-  </div>
- 
-  </div>
+<div className="row mynkRow" >
+<div className="col-2 left">
+  <ul>
+    <li id="about" onMouseOver={updateAbout}>ABOUT</li>
+    <li id="projects" onMouseOver={updateProject}>PROJECTS</li>
+    <li id="resume" onMouseOver={updateResume}>RESUME</li>
+    <li id="contact" onMouseOver={updateContact}>CONTACT</li>
+  </ul>
 </div>
+<div className="col-2 right" id="description">
+<p>
+  <h1>ABOUT</h1>
+  Lorem ipsum dolor sit amet. Qui galisum enim At fugiat consequuntur sit modi quam. Aut quaerat iusto sit totam odio aut neque facilis et nesciunt blanditiis est voluptatem mollitia aut neque praesentium sed impedit aperiam. Ut reiciendis quas aut accusantium repudiandae non quaerat tenetur est quia rerum ut voluptas sint qui dolor officiis est quasi omnis? Eos adipisci dolore non eius assumenda aut praesentium galisum in molestiae alias aut soluta expedita et quia iusto.
+</p>
+</div>
+</div>
+
     </div>
+    
   );
 }
 
